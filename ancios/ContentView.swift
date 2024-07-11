f89c905337bd6fe57cbb52fce3415710b0501960
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-07-11 14:52:02 -0700

    Show imported logo image on the screen

--- ancios/ContentView.swift
+++ ancios/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(uiImage: .logo)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
         }
         .padding()
     }
