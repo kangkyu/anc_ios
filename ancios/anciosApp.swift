45cd3939825dfb51d9d0c69569f465ce8ee0c839
Kang-Kyu Lee <kangkyu1111@gmail.com>
2024-07-11 13:36:51 -0700

    Initial Commit

---
+++ ancios/anciosApp.swift
@@ -0,0 +1,17 @@
+//
+//  anciosApp.swift
+//  ancios
+//
+//  Created by Kang-Kyu Lee on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct anciosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
